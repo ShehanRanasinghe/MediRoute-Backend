a1298767c37a02159532496ae024146684b852a2
ShehanRanasinghe <205064495+ShehanRanasinghe@users.noreply.github.com>
2026-08-10 21:59:37 +0530

    Initialize Spring Boot project structure

---
+++ src/main/java/com/mediroute/mediroutebackend/MedirouteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mediroute.mediroutebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedirouteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedirouteBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mediroute/mediroutebackend/MedirouteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediroute.mediroutebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedirouteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
